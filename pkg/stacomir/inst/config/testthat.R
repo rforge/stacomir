63d149e218d769e34278e6f25fec48adafdc6922
legrand <legrand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-09-16 11:35:40 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/stacomir@210 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/stacomir/inst/config/testthat.R
+++ pkg/stacomir/inst/config/testthat.R
@@ -1,4 +1,17 @@
 require(testthat)
 test_dir("F:/workspace/stacomir/pkg/stacomir/inst/tests/testthat")
+getUsername <- function(){
+	name <- Sys.info()[["user"]]
+	return(name)
+}
+if(getUsername() == 'cedric.briand')
+{
+	test_dir("F:/workspace/stacomir/pkg/stacomir/inst/tests/testthat")
+}
+if(getUsername() == 'marion.legrand')
+{
+	test_dir("C:/Users/logrami/workspace/stacomir/pkg/stacomir/inst/tests/testthat")
+}
+
 
 
