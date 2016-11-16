3b68fc2442458c7d2db43a68ce68a51e025f402e
briand <briand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-11-16 09:07:59 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/stacomir@234 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/stacomir/inst/config/generate_Roxygen2.R
+++ pkg/stacomir/inst/config/generate_Roxygen2.R
@@ -2,33 +2,35 @@
 
 install.packages("roxygen2")
 install.packages("Rd2roxygen") # reverse documentation
-####################"
-# reversing documentation
-######################"
-require(Rd2roxygen)
-setwd("F:/workspace/stacomir/branch0.5/")
-Rd2roxygen(pkg="stacomir")
-#########"
-# pour les classes S4
-##########
-files<-list.files("F:/workspace/stacomir/branch0.5/stacomir/man")
-files<-files[grep("class",files)]
-for (i in 1:length(files)){
-parse_and_save(stringr::str_c("F:/workspace/stacomir/branch0.5/stacomir/man/",files[i]),
-		gsub(".rd","",stringr::str_c("F:/temp/",files[i])))
-}
-#########################""
-# Building documentation
-######################
-#use either :
-require(devtools)
-document("F:/workspace/stacomir/branch0.5/stacomir")
-# or :
-#vignette("roxygen2")
-setwd("F:/workspace/stacomir/pkg/stacomir")
-data("bMM_Arzal")
-data("bM_Arzal")
-data("msg")
-envir_stacomi <- new.env(parent = emptyenv())
+#####################"
+## reversing documentation
+#######################"
+#require(Rd2roxygen)
+#setwd("F:/workspace/stacomir/branch0.5/")
+#Rd2roxygen(pkg="stacomir")
+##########"
+## pour les classes S4
+###########
+#files<-list.files("F:/workspace/stacomir/branch0.5/stacomir/man")
+#files<-files[grep("class",files)]
+#for (i in 1:length(files)){
+#parse_and_save(stringr::str_c("F:/workspace/stacomir/branch0.5/stacomir/man/",files[i]),
+#		gsub(".rd","",stringr::str_c("F:/temp/",files[i])))
+#}
+##########################""
+## Building documentation
+#######################
+##use either :
+#require(devtools)
+#document("F:/workspace/stacomir/branch0.5/stacomir")
+## or :
+##vignette("roxygen2")
+#setwd("F:/workspace/stacomir/pkg/stacomir")
+#data("bMM_Arzal")
+#data("bM_Arzal")
+#data("msg")
+#envir_stacomi <- new.env(parent = emptyenv())
+#
+
 require(roxygen2)
 roxygen2::roxygenise("F:/workspace/stacomir/pkg/stacomir");warnings()[1:10]
