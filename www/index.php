9745e82663d4279bdd23b2ad70a2399db945f9c8
timothee_besse <timothee_besse@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-06-28 08:10:01 +0000

    maj page web
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/stacomir@436 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -3,25 +3,25 @@
 <!-- It can be changed as liked or replaced by other content -->
 
 <?php
-
-$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
-$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
-$themeroot='http://r-forge.r-project.org/themes/rforge/';
-
-echo '<?xml version="1.0" encoding="UTF-8"?>';
+	
+	$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
+	$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
+	$themeroot='http://r-forge.r-project.org/themes/rforge/';
+	
+	echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 <!DOCTYPE html
 	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
-
-  <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title><?php echo $group_name; ?></title>
-	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+	
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title><?php echo $group_name; ?></title>
+		<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
 		
-<!-- Style Material Design -->
+		<!-- Style Material Design -->
 		<link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.indigo-pink.min.css">
 		<script src="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.min.js"></script>
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
@@ -63,6 +63,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 				<nav class="demo-navigation mdl-navigation mdl-color--blue-grey-800">
 					<a class="mdl-navigation__link" href="https://r-forge.r-project.org/projects/stacomir/"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i>R-forge page</a>
 					<a class="mdl-navigation__link" href="https://r-forge.r-project.org/R/?group_id=1019"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">archive</i>R packages</a>
+					<a class="mdl-navigation__link" href="https://www.rdocumentation.org/packages/stacomiR/" target="_blank"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">archive</i>Documentation</a>
 					<a class="mdl-navigation__link" href="https://r-forge.r-project.org/scm/?group_id=1019"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">autorenew</i>SCM</a>
 					<a class="mdl-navigation__link" href="https://groups.google.com/forum/#!forum/stacomi"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">forum</i>Google group</a>
 					<a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">flag</i>Updates</a>
@@ -75,13 +76,10 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 				<div class="mdl-grid demo-content">
 					<div class=" mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
 						<div class="mdl-card__supporting-text mdl-color-text--grey-600">
-							<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
-								$contents = '';
-								while (!feof($handle)) {
-									$contents .= fread($handle, 8192);
-								}
-								fclose($handle);
-							echo $contents; } ?>
+							<p>stacomiR stands for migratory control stations. It enables to read informations in a database dedicated to the monitoring of fish migrations.It allows to build overviews of fish migrations and create charts with data.</p>
+							<img src="images/stacomi_logo.png" width="150" style="max-width: 100%;"> 
+							<img src="images/Logo.png" width="150" style="max-width: 100%;"> 
+							<img src="images/logo_AFB.png" width="200" style="max-width: 100%;"> 
 						</div>
 					</div>
 					<div class="mdl-shadow--2dp mdl-color--white mdl-cell mdl-cell--8-col">
@@ -118,25 +116,51 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 										echo $feedlist->display(9,"Activity");
 									?>
 									<div class="mdl-card__actions mdl-card--border">
-										<a href="https://r-forge.r-project.org/activity/?group_id=1019" class="mdl-button mdl-js-button mdl-js-ripple-effect">View activity</a>
+									<a href="https://r-forge.r-project.org/activity/?group_id=1019" class="mdl-button mdl-js-button mdl-js-ripple-effect">View activity</a>
 									</div>
-								</div>
-																<div class="demo-separator mdl-cell--1-col"></div>-->
-								<div class="demo-options mdl-card mdl-color--blue-grey-800 mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--3-col-tablet mdl-cell--12-col-desktop">
-									<div class="mdl-card__supporting-text mdl-color-text--blue-grey-50">
-										<h3>Links</h3>
+									</div>
+								<div class="demo-separator mdl-cell--1-col"></div>-->
+								<div class="demo-options mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--3-col-tablet mdl-cell--12-col-desktop">
+									<div class="mdl-card__title mdl-card--expand mdl-color--blue-grey-400">
+										<h2 class="mdl-card__title-text">Download packages</h2>
+									</div>
+									<div class="mdl-card__supporting-text mdl-color-text--grey-600">
+										<h3><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">archive</i> StacomiR</h3>										
+										Fish Migration Monitoring
 										<ul class="mdl-list">
-											<li class="mdl-list__item">
-												<a href="http://www.eptb-vilaine.fr/site/index.php/les-migrateurs/stacomi/presentation" target="_blank">EPTB Vilaine</a>
-											</li>
-										</ul>
-									</div>
-								</div>								
+											<li class="mdl-list__item"><span class="mdl-list__item-primary-content"><a href="https://r-forge.r-project.org/R/?group_id=1019">Latest (R-forge)</a></span></li>
+											<li class="mdl-list__item"><span class="mdl-list__item-primary-content"><a href="http://cran.at.r-project.org/package=stacomiR" target="_blank">Stable (CRAN)</a></span></li>
+											<li class="mdl-list__item"><span class="mdl-list__item-primary-content"><a href="https://cran.r-project.org/web/packages/stacomiR/stacomiR.pdf" target="_blank">Reference manual</a></span></li>
+										</li>
+									</ul>
+									
+									<h3><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">archive</i> Stacomirtools</h3>
+									ODBC Connection Class for Package stacomiR
+									<ul class="mdl-list">
+										<li class="mdl-list__item"><span class="mdl-list__item-primary-content"><a href="https://r-forge.r-project.org/R/?group_id=1019">Latest (R-forge)</a></span></li>
+										<li class="mdl-list__item"><span class="mdl-list__item-primary-content"><a href="http://cran.at.r-project.org/package=stacomirtools" target="_blank">Stable (CRAN)</a></span></li>
+										<li class="mdl-list__item"><span class="mdl-list__item-primary-content"><a href="https://cran.r-project.org/web/packages/stacomirtools/stacomirtools.pdf" target="_blank">Reference manual</a></span></li>
+									</ul>
+								</div>
 							</div>
+							<div class="demo-separator mdl-cell--1-col"></div>
+							<div class="demo-options mdl-card mdl-color--blue-grey-800 mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--3-col-tablet mdl-cell--12-col-desktop">
+								<div class="mdl-card__supporting-text mdl-color-text--blue-grey-50">
+									<h3>Projects using StacomiR</h3>
+									<ul class="mdl-list">
+										<li class="mdl-list__item">
+											<span class="mdl-list__item-primary-content"><a href="http://www.eptb-vilaine.fr/site/telechargement/migrateurs/Pechelec_2014-2015.pdf" target="_blank"><i class="material-icons">description</i>Briand, Sauvaget, Eriau 2016</a></span>
+											<li class="mdl-list__item">
+												<span class="mdl-list__item-primary-content"><a href="mailto:cedric.briand@eptb-vilaine.fr" target="_blank"><i class="material-icons">note_add</i>I'm using it</a></span>
+											</li>
+										</li>
+									</ul>
+								</div>
+							</div>								
 						</div>
-					</main>
-				</div>
-				<script src="$$hosted_libs_prefix$$/$$version$$/material.min.js"></script>
-			</body>
-		</html>
-		
+					</div>
+				</main>
+			</div>
+			<script src="$$hosted_libs_prefix$$/$$version$$/material.min.js"></script>
+		</body>
+	</html>
