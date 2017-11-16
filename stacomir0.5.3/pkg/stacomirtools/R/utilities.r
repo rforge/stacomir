b016f33824af3f4a5de37b203530abfc95c86fad
briand <briand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-11-16 13:41:24 +0000

    was reporting logical not the column name
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/stacomir@465 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- stacomir0.5.3/pkg/stacomirtools/R/utilities.r
+++ stacomir0.5.3/pkg/stacomirtools/R/utilities.r
@@ -52,7 +52,7 @@ chnames=function(object,
 		new_variable_name){
 		if (length(old_variable_name)!=length(new_variable_name)) stop("les variables de remplacement doivent avoir le meme nombre que les variables de depart")
 		if (!all(!is.na(match(old_variable_name,colnames(object))))) {
-		   stop(paste("les noms",paste(is.na(match(old_variable_name,colnames(object))),collapse="/"),"ne correspondent pas aux variables du tableau"))
+		   stop(paste("les noms",paste(match(old_variable_name,colnames(object))[is.na(match(old_variable_name,colnames(object)))],collapse="/"),"ne correspondent pas aux variables du tableau"))
     }
 	colnames(object)[match(old_variable_name,colnames(object))]<- new_variable_name
 	return(object)
