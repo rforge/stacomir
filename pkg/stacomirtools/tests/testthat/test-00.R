4713a197292e40a9a5a8cb195dc90f0966e4f25e
briand <briand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-09-23 12:41:08 +0000

    adapted test
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/stacomir@510 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/stacomirtools/tests/testthat/test-00.R
+++ pkg/stacomirtools/tests/testthat/test-00.R
@@ -4,6 +4,7 @@ context("RequeteODBC")
 test_that("Test that RequeteODBC works on a database ", {
         skip_on_cran()
         # this requires an odbc link to be setup for test here a database bd_contmig_nat used for stacomir
+        # the odbc link is bd_contmig_nat and points to database bd_contmig_nat
         # userlocal and passwordlocal are generated from Rprofile.site
         require(stacomirtools)
         req<-new("RequeteODBC")
@@ -20,8 +21,7 @@ test_that("Test that RequeteODBC returns the sql string when envir_stacomi and s
       assign("showmerequest",1,envir_stacomi)
       req<-new("RequeteODBC")
       req@sql<-"select * from iav.t_lot_lot limit 10"
-      req@baseODBC <- c("bd_contmig_nat",userlocal,passwordlocal)
-      req<-connect(req)      
-      expect_that(req@connection,is_a("RODBC"))
+      req@baseODBC <- c("bd_contmig_nat",userlocal,passwordlocal)   
+      expect_output(connect(req),"select \\* from iav\\.t_lot_lot limit 10")
     })
 
